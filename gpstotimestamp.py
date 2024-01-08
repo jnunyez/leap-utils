2db1935bf90fdf7ebca4e73bddaa83a194a33eef
Jose Nuñez <jnunyez@users.noreply.github.com>
2024-01-08 16:42:09 +0100

    gps time to ntptimestamp

---
+++ gpstotimestamp.py
@@ -0,0 +1,24 @@
+import datetime, calendar
+
+datetimeformat = "%Y-%m-%d %H:%M:%S"
+
+# ubxtool  -t -p NAV-TIMELS -P 29.20
+# UBX-NAV-TIMELS:
+#  iTOW 144529000 version 0 reserved2 0 0 0 srcOfCurrLs 2
+#  currLs 18 srcOfLsChange 2 lsChange 0 timeToLsEvent -66672511
+#  dateOfLsGpsWn 2185 dateOfLsGpsDn 7 reserved2 0 0 0
+#  valid x3
+
+# after reading from ubxtool dateOfLsGpsWn
+gpsWn=2185
+
+# after reading from ubxtool dateOfLsGpsDn
+gpsDn=7
+
+epoch = datetime.datetime.strptime("1980-01-06 00:00:00",datetimeformat)
+elapsed = datetime.timedelta(days=(gpsWn*7), seconds=(gpsDn*86400))
+
+#absolutetime
+print(datetime.datetime.strftime(epoch+elapsed, datetimeformat))
+#timestamp
+print(datetime.datetime.timestamp(epoch+elapsed))
